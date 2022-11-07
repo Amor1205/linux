aa9e0d2ac2d86129536079c40aeb988954b0373e
amor <453177150@qq.com>
2022-11-08 00:14:44 +0800

    polymorphism

---
+++ c4/code/.ccls-cache/@home@amor@linux@linux@c4@code/t.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+	public:
+		virtual void BuyTicket(){cout << "full ticket" << endl;}
+};
+class Student: public Person{
+	public:
+		virtual void BuyTicket(){cout << "half ticket" << endl;}
+};
+void Func(Person& p)
+{
+	p.BuyTicket();
+}
+
+class A 
+{
+private:
+	A(int a = 0)
+		:_a(a){}
+public:
+	static A create_obj(int a = 0)
+	{
+		return A(a);
+	}
+protected:
+	int _a;
+};
+
+class B: public A
+{
+
+};
+
+class Car{
+	public:
+		virtual void Drive() = 0;
+};
+
+class Base{
+public:
+	virtual void Func1(){
+		cout << "Func1()" << endl;
+	}
+private:
+	int _b = 1;
+	char _ch = 'a';
+};
+
+int main()
+{
+//	Person ps;
+//	Student st;  
+//	Func(ps);
+//	Func(st);	
+//	A aa = A::create_obj(10);	
+
+//	cout << sizeof(Base)  << endl;
+
+	Person Mike;
+	Func(Mike);
+
+	Student Johnson;
+	Func(Johnson);
+
+	return 0;
+}std
