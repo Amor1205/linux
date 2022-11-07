66a01d98597f093276ed0c169588ff4e5faeafda
amor <453177150@qq.com>
2022-11-07 13:24:12 +0800

    string_homework_final

--- homework/2_string/s.cc
+++ homework/2_string/s.cc
@@ -90,18 +90,44 @@ namespace amor
 
     // modify
 
-    void push_back(char c);
+    void push_back(char c){
+	if(_size == _capacity)
+	{
+		reserve(_capacity == 0 ? 4 : _capacity * 2);
 
-    string& operator+=(char c);
+	}
+	_str[_size] = c;
+	++_size;
+	_str[_size] = '\0';
+	
+    }
 
-    void append(const char* str);
+    string& operator+=(char c){
+	    push_back(c);
+	    return *this;
+    }
+    void append(const char* str){
+	    size_t len = strlen(str);
+	    if(_size+ len >= _capacity)
+	    {
+		    reserve(_size + len);
 
-    string& operator+=(const char* str);
+	    }
+	    strcpy(_str+ _size, str);
+	    _size += len;
 
-    void clear();
+    }
+    string& operator+=(const char* str){
+	    append(str);
+	    return *this;
+    }
+    void clear(){
+	    _str[0] = '\0';
+	    _capacity = _size = 0;
+    }
 
 
-    const char* c_str()const{
+    const char* c_str()const {
 	    return _str;
     }
 
@@ -121,10 +147,33 @@ namespace amor
 	    return _size==0;
     }
 
-    void resize(size_t n, char c = '\0');
+    void resize(size_t n, char c = '\0'){
+	    if(n < _size)
+	    {
+		    _size = n;
+		    _str[_size] = '\0';
+	    }
+	    else{
+		    if(n > _capacity)
+		    {
+			    reserve(n);
+		    }
+		    memset(_str + _size, c , n - _size);
+		    _size = n;
+		    _str[_size] = '\0';
+	    }
+    }
+    void reserve(size_t n){
+	    if(n > _capacity)
+	    {
+		    char* tmp = new char[n+1];
+		    strcpy(tmp,_str);
+		    delete[] _str;
 
-    void reserve(size_t n);
-
+		    _str = tmp;
+		    _capacity = n;
+	    }
+    }
 
     /////////////////////////////////////////////////////////////
 
@@ -143,39 +192,123 @@ namespace amor
 
     //relational operators
 
-    bool operator<(const string& s);
-
-    bool operator<=(const string& s);
-
-    bool operator>(const string& s);
-
-    bool operator>=(const string& s);
-
-    bool operator==(const string& s);
-
-    bool operator!=(const string& s);
+    bool operator<(const string& s){
+	    return !(*this > s);
+    }
+    bool operator<=(const string& s){
+	    return !(*this > s);
+    }
+    bool operator>(const string& s){
+	if(strcmp(_str, s._str))
+		return true;
+	else{
+		return false;
+	}
+    }
+    bool operator>=(const string& s){
+	    return (*this > s || *this == s);
+    }
+    bool operator==(const string& s){
+	    if(strcmp(_str,s._str)==0)
+		    return true;
+	    return false;
+    }
+    bool operator!=(const string& s){
+	    return !(*this == s);
+    }
 
 
     // 返回c在string中第一次出现的位置
 
-    size_t find (char c, size_t pos = 0) const;
+    size_t find (char c, size_t pos = 0) const{
+	    assert(pos <= _size && pos >= 0);
+	    while(pos<=_size)
+	    {
+		    if(_str[pos] == c)
+			    return pos;
+		    else{
+			    ++pos;
+		    }
+	    }
+	    return std::string::npos;
+    }
 
     // 返回子串s在string中第一次出现的位置
 
-    size_t find (const char* s, size_t pos = 0) const;
-
+    size_t find (const char* s, size_t pos = 0) const{
+	    assert(pos <= _size && pos >= 0);
+	    const char* ptr =  strstr(_str + pos, s);
+	    if( ptr == nullptr)
+	    {
+		    return std::string::npos;
+	    }
+	    else{
+		    return ptr - _str;
+	    }
+	    
+    }
     // 在pos位置上插入字符c/字符串str，并返回该字符的位置
 
-    string& insert(size_t pos, char c);
+    string& insert(size_t pos, char c){
+	assert(pos <= _size && pos >= 0);
+	if(_size == _capacity)
+	{
+		reserve(_capacity == 0 ? 4 : 2 * _capacity);
+	}	
+	if(pos == _size)
+	{
+		push_back(c);
+	}
+	else{
+		size_t end = _size + 1;
+		while( end > pos )
+		{
+			_str[end] = _str[end - 1];
+			-- end;
+		}
+		_str[pos] = c;
+		++_size;
+	}
+	return *this;
+    }
+    string& insert(size_t pos, const char* str){
+	assert(pos <= _size && pos >= 0);
+	size_t len = strlen(str);
+	if(_size + len > _capacity)
+	{
+		reserve(_size + len);
+	}
+	if(pos == _size)
+	{
+		append(str);
+	}
+	else{
+		size_t end = _size + len;
+		while(pos < end)
+		{
+			_str[end] = _str[end - len];
+			-- end;
+		}
+		memcpy(_str + pos, str, len);
+		_size += len;
+		return *this;
+	}
 
-    string& insert(size_t pos, const char* str);
-
-     
+    }     
 
     // 删除pos位置上的元素，并返回该元素的下一个位置
 
-    string& erase(size_t pos, size_t len);
-
+    string& erase(size_t pos = 0, size_t len = std::string::npos){
+	if(len == std::string::npos || len + pos >= _size)
+	{
+		_str[pos] = '\0';
+		_size = pos;
+	}
+	else{
+		strcpy(_str + pos, _str + pos + len);
+		_size -= len;
+	}
+    }
   private:
 
     char* _str;
@@ -209,20 +342,23 @@ std::istream& operator>>(std::istream is, amor::string& s)
 int main()
 {
 	amor::string s("hello world");
-	/*for(auto ch : s)
+	s.insert(0,"hello??????");
+	s.erase(5,6);
+	for(auto ch : s)
 	{
-		std::cout << ch << " ";
+		std::cout << ch;
 	}
-	std::cout << std::endl;
-	*/
+	std::cout << " ";
+	std::cout << s.find('o');	
 	//std::cout << s ;
-	amor::string::iterator it = s.begin();
+/*	amor::string::iterator it = s.begin();
 	while(it != s.end())
 	{
-		std::cout << *it << " ";
+		std::cout << *it;
 		++it;
 	}
-	std::cout << std::endl;
-
+	std::cout << " ";
+	//std::cout << std::endl;
+*/
 	return 0;
 }
