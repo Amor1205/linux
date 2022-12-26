53077089f35b6feda1528b252f3029808226519a
Amor1205 <453177150@qq.com>
2022-12-26 21:49:26 +0800

    C++11

---
+++ c11/c++11.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+struct Point
+{
+    int _x;
+    int _y;
+};
+
+int main()
+{
+    int array1[] = { 1, 2, 3, 4};
+    int array2[5] = { 0 };
+    Point pp1 = {1,2};
+    
+}
