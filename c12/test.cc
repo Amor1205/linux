98d933eb5f7dd3f1f21fcea752ead9b057b2a89b
Amor1205 <453177150@qq.com>
2022-12-30 01:49:54 +0800

    c12

--- c12/test.cc
+++ c12/test.cc
@@ -1,7 +1,36 @@
 #include<iostream>
+#include<string>
+using namespace std;
+class Exception
+{
+public:
+    Exception(const string& errmsg, int id)
+        :_errmsg(errmsg)
+        ,_id(id)
+        {}
+    const string& what()
+    {
+        return _errmsg;
+    }
+protected:
+    string _errmsg;
+    int _id;
+};
+void ServerStart()
+{
+    try{
+    }
+    catch()
+    {
 
+    }
+    catch(...)
+    {
+        
+    }
+}
 int main()
 {
+    ServerStart();
 
-    cout << "hello,world!";
 }
