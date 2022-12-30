ac3dd96c156609f498f51b45df4f91b41e8362ad
Amor1205 <453177150@qq.com>
2022-12-30 23:38:22 +0800

    exception

--- c12/test.cc
+++ c12/test.cc
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<time.h>
+#include<vector>
+
 using namespace std;
 class Exception
 {
@@ -8,7 +11,7 @@ public:
         :_errmsg(errmsg)
         ,_id(id)
         {}
-    const string& what()
+    const string& what() const
     {
         return _errmsg;
     }
@@ -16,21 +19,77 @@ protected:
     string _errmsg;
     int _id;
 };
+
+class func1_exception : public Exception
+{
+
+};
+class func2_exception : public Exception
+{
+
+};
+class func3_exception : public Exception
+{
+public:
+    func3_exception(const string& errmsg, int id, const string& type)
+        :Exception(errmsg,id),_type(type)
+    {    
+    }
+
+private:
+    const string& _type;
+};
+void Func1()
+{
+    
+}
+void Func2()
+{
+    Func1();
+}
+void Func3()
+{
+    if(rand()%2 == 0)
+    {
+        throw func3_exception("connection error", 100, "get");
+    }
+    else if (rand()%7 == 0)
+    {
+        throw func3_exception("permission error", 200, "post");
+    }
+    Func2();
+}
 void ServerStart()
 {
-    try{
-    }
-    catch()
+    srand(time(0));
+    int n = 100;
+    while(n--)
     {
+        try{
+            Func3();
+        }
+        catch(const Exception& e)
+        {
+            cout << e.what() << endl;
+        }
+        catch(...)
+        {
+            cout << "unkown Exception" << endl;
+        }
+    }
 
-    }
-    catch(...)
-    {
-        
-    }
 }
+// int main()
+// {
+//     ServerStart();
+
+// }
+
+
 int main()
 {
-    ServerStart();
-
+    vector<int> v(10,5);
+    // v.reserve(1000000000000);
+    v.at(10) = 0;
+    return 0;
 }
