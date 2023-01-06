9eb559a4713ea63e260a4a5b469f816c7c65a225
Amor1205 <453177150@qq.com>
2023-01-07 01:23:18 +0800

    sort_day_2

--- r1/sort.h
+++ r1/sort.h
@@ -2,6 +2,9 @@
 #include<vector>
 #include<string>
 #include<iostream>
+#include<algorithm>
+#include<queue>
+
 using namespace std;
 
 //direct insertion sort
@@ -53,4 +56,81 @@ void shell_sort(vector<int>& v)
         }
     }
 
+}
+
+void direct_selection_sort(vector<int>& v)
+{
+    auto it = v.begin();
+    for(int i = 0; i < v.size()-1; ++i)
+    {
+        auto pmin = min_element(it,v.end());
+        if(v[i] != *pmin)
+        {
+            swap(v[i],*pmin);
+        }
+        ++it;
+    }
+}
+
+void heap_sort(vector<int>& v)
+{
+    priority_queue<int> pq;
+    for(auto e : v)
+    {
+        pq.push(e);
+    }
+    for(int i = 0; i < v.size()-1; ++i)
+    {
+        if(v[i] != pq.top())
+        {
+            v[i] = pq.top();
+        }
+        pq.pop();
+    }
+}
+
+void Swap(int* p1, int* p2)
+{
+    int tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+}
+
+void adjust_down(int* a,int n, int parent)
+{
+    int min_child = parent*2 + 1;
+    while(min_child < n)
+    {
+        if(min_child+1 < n && a[min_child+1] < a[min_child])
+        {
+            ++min_child;
+        }
+        if(a[min_child] > a[parent])
+        {
+            Swap(&a[min_child], &a[parent]);
+            parent = min_child;
+            min_child = parent * 2 + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+void c_heap_sort(int* a, int n)
+{
+    //form heap
+    for(int i = (n - 1 - 1)/2; i >= 0; --i)
+    {
+        adjust_down(a,n,i);
+    }
+    //choose number
+    int i = 1;
+    while(i < n)
+    {
+        Swap(&a[0], &a[n-i]);
+        adjust_down(a,n-i,0);
+        ++i;
+    }
 }
--- r1/test.cc
+++ r1/test.cc
@@ -4,14 +4,21 @@
 int main()
 {
     vector<int> v;
-    int a[] = {5,3,2,7,9,11,1};
+    int a[] = {5,3,2,5,7,9,11,1};
     for(auto e : a)
     {
         v.push_back(e);
     }
     //direct_insert_sort(v);
-    shell_sort(v);
-    for(auto e : v)
+    //shell_sort(v);
+    //direct_selection_sort(v);
+    //heap_sort(v);
+    c_heap_sort(a,8);//wrong
+    // for(auto e : v)
+    // {
+    //     cout << e << " ";
+    // }
+    for(auto e : a)
     {
         cout << e << " ";
     }
